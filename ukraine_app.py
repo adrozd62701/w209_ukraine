8b5eb0d3d1628194d06c508a02da5d527d08b04f
Alice Drozd <adrozd62701@berkeley.edu>
2025-04-15 18:39:09 -0700

    adding authors

--- ukraine_app.py
+++ ukraine_app.py
@@ -56,11 +56,14 @@ tab1, tab2, tab3, tab4, tab5 = st.tabs(["Home", "Conflict Map","Human Cost","Gro
 with tab1:
     st.header("Welcome to our app!")
     st.write("This app tells the story of the conflict in Ukraine through data visualizations.")
-    st.write("Our data comes from the [Armed Conflict Location & Event Data (ACLED)](https://acleddata.com). The ACLED is a disaggregated data collection,"
+
+    st.write("**Authors:** Akanksha Chattopadhyay, Alice Drozd, Sooyeon Kim, Rina Palta" )
+
+    st.write("**Data:** Our data comes from the [Armed Conflict Location & Event Data (ACLED)](https://acleddata.com). The ACLED is a disaggregated data collection,"
         "analysis, and crisis mapping initiative. ACLED collects information on the dates, actors, locations, fatalities, "
         "and types of all reported political violence and protest events around the world.")
     st.write("Please explore the interactive visualizations on our website:")
-    st.write(
+    st.write("**Conflict Map:** "
         "This animated map shows the general progression of conflict intensity across Ukraine "
         "from the beginning of the war through today. Each frame represents a monthly snapshot, "
         "with conflict density shown in blue and fatality events marked in dark blue."
